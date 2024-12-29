249322be0ebf0028a75608a2757860cf4dbf5e50
Hamza-Aglagal <hamzaaglagal7@gmail.com>
2024-12-29 19:10:13 +0100

    first commit

---
+++ src/components/Feedbacks.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const Feedbacks = () => {
+  return (
+    <div className={`mt-12 bg-black-100 rounded-[20px]`}>
+    
+    </div>
+  );
+};
+
+export default Feedbacks;
